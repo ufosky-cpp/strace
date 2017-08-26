2f69b3617ac2894228f735a577f2c9293256dc6e
Nikolay Marchuk <marchuk.nikolay.a@gmail.com>
2017-08-26 20:50:25 +0700

    Introduce new filtering architecture
    This change introduces new filtering architecture primitives: filter,
    filter_action and bool_expression. Filtering is now done after decoding of
    syscall and tcp->qual_flg stores filtering results.
    
    * basic_actions.c: New file.
    * filter_action.c: Likewise.
    * filter_expression.c: Likewise.
    * filter.c: Likewise.
    * basic_filters.c (parse_syscall_filter, run_syscall_filter,
    free_syscall_filter): New functions.
    * defs.h (filter_syscall, filtering_parsing_finish): Add new declarations.
    * filter.h: Add new declarations.
    * filter_qualify.c (abbrev_set, raw_set, trace_set, verbose_set):
    Remove set variables.
    (qualify_trace, qualify_abbrev, qualify_verbose, qualify_raw):
    Use new filtering API.
    (qual_flags): Remove QUAL_* flags for trace, abbrev, verbose, raw.
    * strace.c (init): Call filtering_parse_finish after command line parsing.
    (trace_syscall): Add filtering after syscall decoding.
    * Makefile.am (strace_SOURCES): Add new files.
    
    [ldv: simplify *_qualify_mode]
    [ldv: eliminate parse_null]
    [ldv: introduce qualify_filter]
    [ldv: use loop initial declarations]

--- strace.c
+++ strace.c
@@ -1749,6 +1749,7 @@ init(int argc, char *argv[])
 			break;
 		}
 	}
+	filtering_parsing_finish();
 
 	argv += optind;
 	argc -= optind;
@@ -2402,6 +2403,7 @@ trace_syscall(struct tcb *tcp, unsigned int *sig)
 		case 0:
 			return 0;
 		case 1:
+			filter_syscall(tcp);
 			res = syscall_entering_trace(tcp, sig);
 		}
 		syscall_entering_finish(tcp, res);
