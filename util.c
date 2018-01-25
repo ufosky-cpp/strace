0ded35699038cc8687840e7a57e510c214351030
Eugene Syromyatnikov <evgsyr@gmail.com>
2018-01-25 17:53:23 +0100

    util.c: print character/block device number in printfd
    * util.c: Include <sys/stat.h>, <sys/sysmacros.h>,
    and "largefile_wrappers.h".
    (printsocket, printdev): New functions.
    (printfd): Move socket matching/printing logic to printsocket.  Check
    also for printdev.
    * strace.1.in: Mention this.
    * NEWS: Likewise.

--- util.c
+++ util.c
@@ -36,10 +36,14 @@
 #include <sys/param.h>
 #include <fcntl.h>
 #include <stdarg.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
 #ifdef HAVE_SYS_XATTR_H
 # include <sys/xattr.h>
 #endif
 #include <sys/uio.h>
+
+#include "largefile_wrappers.h"
 #include "xstring.h"
 
 int
@@ -418,22 +422,54 @@ getfdinode(struct tcb *tcp, int fd)
 	return 0;
 }
 
+bool
+printsocket(struct tcb *tcp, int fd, const char *path)
+{
+	const char *str;
+	size_t len;
+	unsigned long inode;
+
+	return ((str = STR_STRIP_PREFIX(path, "socket:[")) != path)
+		&& (len = strlen(str))
+		&& (str[len - 1] == ']')
+		&& (inode = strtoul(str, NULL, 10))
+		&& print_sockaddr_by_inode(tcp, fd, inode);
+}
+
+bool
+printdev(struct tcb *tcp, int fd, const char *path)
+{
+	struct_stat st;
+
+	if (path[0] != '/')
+		return false;
+
+	if (stat_file(path, &st)) {
+		debug_func_perror_msg("stat(\"%s\")", path);
+		return false;
+	}
+
+	switch (st.st_mode & S_IFMT) {
+	case S_IFBLK:
+	case S_IFCHR:
+		tprintf("%s:[%s %u:%u]",
+			path, S_ISBLK(st.st_mode)? "block" : "char",
+			major(st.st_rdev), minor(st.st_rdev));
+		return true;
+	}
+
+	return false;
+}
+
 void
 printfd(struct tcb *tcp, int fd)
 {
 	char path[PATH_MAX + 1];
 	if (show_fd_path && getfdpath(tcp, fd, path, sizeof(path)) >= 0) {
-		const char *str;
-		size_t len;
-		unsigned long inode;
-
 		tprintf("%d<", fd);
 		if (show_fd_path <= 1
-		    || (str = STR_STRIP_PREFIX(path, "socket:[")) == path
-		    || !(len = strlen(str))
-		    || str[len - 1] != ']'
-		    || !(inode = strtoul(str, NULL, 10))
-		    || !print_sockaddr_by_inode(tcp, fd, inode)) {
+		    || (!printsocket(tcp, fd, path)
+		         && !printdev(tcp, fd, path))) {
 			print_quoted_string(path, strlen(path),
 					    QUOTE_OMIT_LEADING_TRAILING_QUOTES);
 		}
